7de01e6bf2f7ae42ae6aac6c9726ce22a394af5a
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-03-04 15:13:21 +0300

    create

---
+++ HW1/HW1T3/HW1T3/Program.fs
@@ -0,0 +1,9 @@
+﻿// Calculates  
+let fibonacci n = 
+    if (n < 0) then raise(System.ArgumentOutOfRangeException())
+    let rec loop first second iterator =
+        if (iterator = 0) then first
+        else loop second (first + second) (iterator - 1)
+    loop 0 1 n
+
+printfn "%A" (fibonacci 7)
