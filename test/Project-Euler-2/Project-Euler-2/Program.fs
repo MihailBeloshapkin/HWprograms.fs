10045513567d7fc00e56c373587141110ba42f6f
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-04-23 15:09:04 +0300

    create

---
+++ test/Project-Euler-2/Project-Euler-2/Program.fs
@@ -0,0 +1,15 @@
+﻿module fibonacci
+
+open System
+
+let fibonacci = 
+    let fibonacciList = []
+    let rec loop fibonacciList first second = 
+        if (first + second > 4000000) then fibonacciList
+        else loop (fibonacciList @ [first + second]) second (first + second)
+  //  List.filter (fun x -> x % 2 = 0) (loop fibonacciList 0 1)
+    List.fold (fun acc i -> acc + i) 0 (List.filter (fun x -> x % 2 = 0) (loop fibonacciList 0 1))  
+
+let listOfNumbers = fibonacci
+
+printfn "%A" listOfNumbers
