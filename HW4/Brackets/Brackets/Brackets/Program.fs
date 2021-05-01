727436c75561e2bab3dc6da6c832ec27cb92a902
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-05-01 21:59:43 +0300

    create

---
+++ HW4/Brackets/Brackets/Brackets/Program.fs
@@ -0,0 +1,40 @@
+﻿open System.Collections.Generic
+
+let isBracket char = 
+    match char with
+    | '(' -> true
+    | ')' -> true
+    | '[' -> true
+    | ']' -> true
+    | '{' -> true
+    | '}' -> true
+    | _ -> false
+
+let getBrackets str = 
+    let rec loop string bracketList = 
+        match string with
+        | head :: tail when isBracket head -> loop tail (bracketList @ [head])
+        | head :: tail -> loop tail bracketList
+        | [] -> bracketList
+    let list = Seq.toList str
+    loop list []
+
+let bracketChecker str = 
+    let brackets = [('(', ')'); ('[', ']'); ('{', '}')]
+    let rec loop (str : list<char>) bracketStack =
+        match str with
+        | head :: tail when brackets |> List.exists (fun x -> fst x = head) ->
+            loop tail (head :: bracketStack)
+        | head :: tail when brackets |> List.exists (fun x -> snd x = head) ->
+            match head with
+            | _ when brackets |> List.findIndex (fun x -> snd x = head) <> (brackets |> List.findIndex (fun x -> snd x = bracketStack.Head)) 
+                -> false
+            | _ -> loop str.Tail bracketStack.Tail
+        | [] -> true
+    let input = Seq.toList str |> getBrackets
+    loop input []
+
+
+let str = "{}[][r]ew["
+
+printfn "%A" (getBrackets str)
