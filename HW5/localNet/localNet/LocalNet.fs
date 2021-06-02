8f97a8bf9ea29b8c9426bd6fc3a1001caa9f8a9d
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-06-02 15:57:37 +0300

    add

--- HW5/localNet/localNet/LocalNet.fs
+++ HW5/localNet/localNet/LocalNet.fs
@@ -2,3 +2,24 @@
 
 open OS
 open Virus
+open PC
+open System
+
+type localNet(pcSet : List<PC>, virus : Virus, random : Random) =
+    member this.turn =
+        pcSet |> List.iter (fun (pc : PC) -> pc.tryInfect(virus, random))
+        List.iter (fun (pc :PC) -> pc.JustInfected <- false) pcSet
+
+    member this.canChange =
+        List.exists (fun (pc : PC) -> pc.canBeInfected(virus)) pcSet
+
+    member this.run =
+        if this.canChange then 
+            this.turn
+            let infectedStr (pc : PC) = if pc.IsInfected then "Infected" else "Not Infected"
+            List.iter (fun (pc : PC) -> printfn $"{pc.Id} with OS {pc.OS} is {infectedStr pc}") pcSet
+            printfn "Press\n"
+            Console.ReadKey |> ignore
+            this.run
+        else 
+            printfn "Process is finished"
--- HW5/localNet/localNet/PC.fs
+++ HW5/localNet/localNet/PC.fs
@@ -12,9 +12,13 @@ type PC(os : OS, id : string) =
     member val Id = id with get
 
     member this.tryInfect(virus : Virus, random : Random) = 
-//        if this.IsInfected |> not then
-//            this.IsInfected <- 
+        if this.IsInfected |> not then
+            this.IsInfected <- this.AdjactedPCs |> List.exists (fun (adjacent : PC) ->
+            (adjacent.IsInfected && (adjacent.JustInfected |> not) 
+            && (random.NextDouble() < virus.Check(this.OS, adjacent.OS)))
+            )
+            if this.IsInfected then this.JustInfected <- true
 
-
-
-
+    member this.canBeInfected(virus : Virus) = 
+        this.IsInfected && List.exists (fun (adjacent : PC) -> (adjacent.IsInfected |> not) && 
+        virus.Check(this.OS, adjacent.OS) > 0.0) this.AdjactedPCs
