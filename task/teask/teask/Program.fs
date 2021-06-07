6916e1a710521b3bb122b1f527ca910d05a4a2bd
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-06-07 08:13:23 +0300

    task

---
+++ task/teask/teask/Program.fs
@@ -0,0 +1,14 @@
+﻿module sumOfDigits
+
+open System
+
+let digit number =
+    let rec loop number acc =
+        match number with
+        | 0 -> acc
+        | _ -> loop (number / 10) (acc + number % 10)
+    loop number 0
+
+digit 123
+
+
