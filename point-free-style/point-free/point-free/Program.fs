b2f5f4db82a2d444c987d7e9f64c93f27248b26a
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-06-07 08:02:21 +0300

    create

---
+++ point-free-style/point-free/point-free/Program.fs
@@ -0,0 +1,11 @@
+﻿module pointTree
+
+open System
+
+let f g l = List.map g (List.tail l)
+
+let f'1 g = List.tail >> List.map g
+
+let f'2 = List.map >> ((<<) List.tail)
+
+printfn "%A" (f'2 (fun x -> x*2) [1; 2; 5])
