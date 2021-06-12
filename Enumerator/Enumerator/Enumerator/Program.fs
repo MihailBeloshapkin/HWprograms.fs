91a861c8a1dda5b362e5d8995a973d198e7cf7b2
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-06-12 22:43:35 +0300

    create and add

---
+++ Enumerator/Enumerator/Enumerator/Program.fs
@@ -0,0 +1,32 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Collections.Generic
+
+type Tree<'a> =
+    | Element of 'a * Tree<'a> * Tree<'a>
+    | Empty
+
+type Data () =
+    let mutable tree : Tree<int> = Tree.Empty
+
+    member this.AddData (newData : int) =
+        let rec adding (tree : Tree<int>, newData : int) =
+            match tree with
+            | Element(data, Empty, more) when newData < data -> Element(data, Tree.Element(newData, Empty, Empty), more)
+            | Element(data, less, more) when newData > data -> adding (more, newData)
+            | Element(data, less, more) when newData < data -> adding (less, newData)
+            | Element(data, less, more) when newData = data -> Element(data, less, more)
+            | Empty -> Element(newData, Empty, Empty)
+        tree <- adding (tree, newData)
+            
+
+    member this.GetData () =
+        1
+
+
+let set = new Data()
+
+set.AddData 5
+set.AddData 7
+    
