b4910a6b6ec593e45551c6d21e7519c424c4120e
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2021-08-01 11:26:37 +0300

    change smth

--- HW7/MiniCrawler/MiniCrawler/Program.fs
+++ HW7/MiniCrawler/MiniCrawler/Program.fs
@@ -5,6 +5,7 @@ open System.IO
 open System.Net
 open System.Text.RegularExpressions
 
+// Get html content from the page.
 let getData (url : string) =
     async {
         try 
@@ -20,16 +21,18 @@ let getData (url : string) =
     }
 
 
-
+// Display data.
 let display url (downloaded : Option<string>) = 
     match downloaded with
     | Some html -> printfn "%s   Length: %d symbols" url html.Length
     | _ -> printfn "Fale"
 
+// Get links from html code.
 let getLinks (html : string) =
     [for matches in (Regex("<a href\s*=\s*\"?(https?://[^\"]+)\"?\s*>", RegexOptions.Compiled).Matches(html) : MatchCollection) 
     -> matches.Groups.[1].Value]
 
+// Download.
 let downloadAll (url : string) =
     let mainData = getData url |> Async.RunSynchronously
     match mainData with
