d8e7c3d705d613593835b74e45988b25f8283eea
Mihail-Beloshapkin <mihail.beloshapkin.mm@yandex.ru>
2021-08-29 22:23:41 +0300

    create and add tests

--- HW3/lambdaInter/lambdaInter/Program.fs
+++ HW3/lambdaInter/lambdaInter/Program.fs
@@ -1,10 +1,10 @@
 ﻿module LambdaInterpreter
 
 // Lambda term.
-type Lambda =
-    | Var of int
-    | Application of Lambda * Lambda
-    | Abstraction of int * Lambda
+type Lambda<'a> =
+    | Var of 'a
+    | Application of Lambda<'a> * Lambda<'a>
+    | Abstraction of 'a * Lambda<'a>
 
 // Get free vars from term.
 let rec getFreeVars term =
@@ -15,6 +15,7 @@ let rec getFreeVars term =
         | Abstraction(var, inner) -> sub inner acc - set[var]
     Set.empty |> sub term
 
+// Get value from the set.
 let rec getNewValFromSet set =
     let newVal = System.Guid.NewGuid()
     if set |> Set.contains newVal then
@@ -22,6 +23,24 @@ let rec getNewValFromSet set =
     else
         newVal
 
-//let rec substitute term variableToChange substitutedTerm =
-//    match term with
-//    | 
+let rec substitute term variableToChange substitutedTerm =
+    match term with
+    | Var name when name = variableToChange -> substitutedTerm
+    | Var _ -> term
+    | Application(left, right) -> Application(substitute left variableToChange substitutedTerm, substitute right variableToChange substitutedTerm)
+    | Abstraction(variable, innerTerm) -> 
+        match variable with 
+        | name when name = variableToChange -> term
+        | _ when getFreeVars innerTerm |> Set.contains variableToChange |> not || getFreeVars substitutedTerm |> Set.contains variable |> not
+            -> Abstraction(variable, substitute innerTerm variableToChange substitutedTerm)
+        | _ -> let newVar = getFreeVars innerTerm + getFreeVars substitutedTerm |> getNewValFromSet 
+               Abstraction(newVar, innerTerm |> substitute (Var newVar) variable |> substitute substitutedTerm variableToChange) 
+
+
+let rec reduce term =
+    match term with
+    | Var _ -> term
+    | Application(left, right) -> match left with
+                                  | Abstraction(variable, innerTerm) -> substitute innerTerm variable right |> reduce
+                                  | _ -> Application(reduce left, reduce right)
+    | Abstraction(variable, innerTerm) -> Abstraction(variable, reduce innerTerm)
--- HW3/lambdaInter/lambdaInterTests/lambdaInterTests.fs
+++ HW3/lambdaInter/lambdaInterTests/lambdaInterTests.fs
@@ -1,11 +1,18 @@
 module lambdaInterTests
 
+open System
 open NUnit.Framework
+open FsUnit
+open LambdaInterpreter
 
 [<SetUp>]
 let Setup () =
     ()
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let ``Simple test`` () =
+    let x = System.Guid.NewGuid()
+    let I x = Abstraction(x, Var x)
+    let sample = Application(I x, I x)
+    reduce sample |> should equal (I x)
+
