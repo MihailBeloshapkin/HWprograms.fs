72f2341ced068387177c440b9fb410e62e530de6
Mihail-Beloshapkin <mihail.beloshapkin.mm@yandex.ru>
2021-09-03 11:20:55 +0300

    fixes

--- HW4/Brackets/Brackets/Brackets/Program.fs
+++ HW4/Brackets/Brackets/Brackets/Program.fs
@@ -1,39 +1,33 @@
 ﻿module Brackets
 
-open System.Collections.Generic
+let brackets = Map.ofList [('(', ')'); ('[', ']'); ('{', '}')]
 
+
+/// Checks that char is bracket.
 let isBracket char = 
     match char with
-    | '(' -> true
-    | ')' -> true
-    | '[' -> true
-    | ']' -> true
-    | '{' -> true
-    | '}' -> true
+    | _ when brackets.ContainsKey(char) || brackets |> Map.toList |> List.map (fun x -> x |> snd) |> List.contains char -> true
     | _ -> false
 
+/// Get list of brackets from the string.
 let getBrackets str = 
     let rec loop string bracketList = 
         match string with
-        | head :: tail when isBracket head -> loop tail (bracketList @ [head])
+        | head :: tail when isBracket head -> loop tail (head :: bracketList)
         | head :: tail -> loop tail bracketList
-        | [] -> bracketList
+        | [] -> bracketList |> List.rev
     let list = Seq.toList str
     loop list []
 
+/// Checks that str is balanced.
 let bracketChecker str = 
-    let brackets = [('(', ')'); ('[', ']'); ('{', '}')]
     let rec loop (str : list<char>) bracketStack =
         match str with
-        | head :: tail when brackets |> List.exists (fun x -> fst x = head) ->
+        | head :: tail when brackets.ContainsKey(head) ->
             loop tail (head :: bracketStack)
-        | head :: tail when brackets |> List.exists (fun x -> snd x = head) ->
-            match str.Head with
-            | _ when bracketStack.Length = 0 -> false
-            | _ when (brackets |> List.findIndex (fun x -> snd x = head)) <> (brackets |> List.findIndex (fun x -> fst x = bracketStack.Head)) 
-                -> false
-            | _ when (brackets |> List.findIndex (fun x -> snd x = head)) = (brackets |> List.findIndex (fun x -> fst x = bracketStack.Head))
-                -> loop str.Tail bracketStack.Tail
-        | [] -> bracketStack.Length = 0
+        | head :: tail -> match bracketStack with
+                          | h :: t when brackets.TryGetValue(h) = (true, head) -> loop tail t
+                          | _ -> false   
+        | [] -> bracketStack.IsEmpty
     let input = Seq.toList str |> getBrackets
     loop input []
