efea18fe816b259b0f8743603884d07191928c69
Mihail Beloshapkin <55751278+MihailBeloshapkin@users.noreply.github.com>
2021-09-06 20:16:24 +0300

    delete

--- HW2/MapForTrees/MapForTrees/Map/Program.fs
+++ HW2/MapForTrees/MapForTrees/Map/Program.fs
@@ -8,14 +8,10 @@ type Tree<'a> =
     | Empty
 
 
-/// <summary>
+/// <summary>HW2T2
 /// Map function for binary trees.
 /// </summary>
 let rec mapForTree func tree =
     match tree with
     | Element(data, less, more) -> Element(func data, mapForTree func less, mapForTree func more)
     | Empty -> Empty 
-
-let a = Empty |> mapForTree (fun x -> x * 2)
-
-printfn "A"
