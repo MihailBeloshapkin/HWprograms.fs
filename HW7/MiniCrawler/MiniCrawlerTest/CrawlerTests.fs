1f3d845edb2e12db51682cfc34261b6bd984efd9
MihailBeloshapkin <mihail.beloshapkin.mm@yandex.ru>
2021-09-17 00:00:01 +0300

    more fixes

--- HW7/MiniCrawler/MiniCrawlerTest/CrawlerTests.fs
+++ HW7/MiniCrawler/MiniCrawlerTest/CrawlerTests.fs
@@ -4,15 +4,12 @@ open NUnit.Framework
 open MiniCrawler
 open FsUnit
 
-let rightResult = [
-  "https://docs.github.com/en/articles/blocking-a-user-from-your-personal-account" 
-  "https://docs.github.com/en/articles/reporting-abuse-or-spam"
-  "https://docs.github.com/categories/setting-up-and-managing-your-github-profile"
-]
 
 [<Test>]
 let ``Simple test`` () =
     downloadAll "https://github.com/MihailBeloshapkin" 
-    |> List.map (fun x -> fst x) |> should equal rightResult
+    |> List.map fst |> should equal [ "https://docs.github.com/en/articles/blocking-a-user-from-your-personal-account" 
+                                      "https://docs.github.com/en/articles/reporting-abuse-or-spam"
+                                      "https://docs.github.com/categories/setting-up-and-managing-your-github-profile" ]
     
 
