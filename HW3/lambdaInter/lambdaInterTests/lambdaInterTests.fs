16d75752a34fdac8c76d08f98c35d708af6ea70f
MihailBeloshapkin <mihail.beloshapkin.mm@yandex.ru>
2021-09-30 12:50:30 +0300

    more fixes

--- HW3/lambdaInter/lambdaInterTests/lambdaInterTests.fs
+++ HW3/lambdaInter/lambdaInterTests/lambdaInterTests.fs
@@ -1,4 +1,4 @@
-module lambdaInterTests
+module LambdaInterTests
 
 open System
 open NUnit.Framework
@@ -6,10 +6,23 @@ open FsUnit
 open LambdaInterpreter
 
 
-[<Test>]
-let ``Simple test`` () =
-    let x = System.Guid.NewGuid()
-    let I x = Abstraction(x, Var x)
-    let sample = Application(I x, I x)
-    reduce sample |> should equal (I x)
+type LambdaInterTest() =
+    static member testTerms() =
+        let x = Guid.NewGuid()
+        let y = Guid.NewGuid()
+        let I x = Abstraction(x, Var x)
+        let K x y = Abstraction(x, Abstraction(y, Var x))
+        let K_s x y = Abstraction(x, Abstraction(y, Var y))
+        [|
+            Application(I x, I x), I x
+            Application(I x, Application(I x, I x)), I x
+            Application(K x y, I x), K_s y x
+            Application(K_s x y, Application(I x, I x)), I y
+        |]
+
+     [<TestCaseSource("testTerms")>]
+     member this.``Simple tests`` (case : Lambda<Guid> * Lambda<Guid>) =
+         case |> fst |> reduce |> should equal (case |> snd)
+
+
 
