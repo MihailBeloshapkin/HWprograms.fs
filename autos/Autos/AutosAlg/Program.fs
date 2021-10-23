88f5ced31cc69d527d26c3afac089a17ea838259
MihailBeloshapkin <mihail.beloshapkin.mm@yandex.ru>
2021-10-23 17:36:12 +0300

    new fixes

--- autos/Autos/AutosAlg/Program.fs
+++ autos/Autos/AutosAlg/Program.fs
@@ -10,9 +10,10 @@ type Response =  Success | NotSuccess | SomethingIsWrong
 type Machine (sizeOfParking : int, occupied : int ref) =
 
     /// When car is trying to get into the parking lot.
-    member this.In () =  match !occupied < sizeOfParking with
-                         | true ->  occupied := !occupied + 1
-                                    Success
+    member this.In () =  let startValue = !occupied
+                         match !occupied < sizeOfParking with
+                         | true ->  let start = Interlocked.CompareExchange(occupied, startValue + 1, startValue)
+                                    if start = startValue then Success else NotSuccess 
                          | false -> NotSuccess
                         
 
@@ -22,24 +23,26 @@ type Machine (sizeOfParking : int, occupied : int ref) =
 /// Manages parking process.
 type Parking (sizeOfPark : int, countOfMachines : int) =
     let occupied = ref 0
-    
-    let locker = Object()
 
     /// Number of occupied.
-    member this.Occupied with get () = !occupied
+    member this.Occupied with get () = occupied
 
     /// Machines that control parking.
     member private this.Machines = [for i in 0 .. countOfMachines do Machine(sizeOfPark, occupied)]
 
     /// Auto In member.
-    member this.AutoIn (number : int) = lock locker (fun () -> if number > -1 && number < this.Machines.Length then this.Machines.[number].In() else SomethingIsWrong)
+    member this.AutoIn (number : int) = if number > -1 && number < this.Machines.Length then this.Machines.[number].In() else SomethingIsWrong
 
     ///  Auto Out member.
     member this.AutoOut (number : int) = if number > -1 && number < this.Machines.Length then this.Machines.[number].Out() else ()
 
+
+// let mutable a = 1
+// Interlocked.CompareExchange(&a, 5, 1) |> ignore
 let park = Parking(2, 5)
 park.AutoIn(0) |> ignore
 let results = [0; 1] |> List.map (fun i -> async { return park.AutoIn(i) })
                      |> Async.Parallel
                      |> Async.RunSynchronously
 printfn "%A" results
+// printfn "%A" a
